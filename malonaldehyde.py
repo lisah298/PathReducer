aa76045da574cab88b5ffe46ba894b5cfa49f839
Lisa Hetzel <lisa_hetzel@freenet.de>
2021-11-01 10:16:07 +0100

    edited data

--- malonaldehyde.py
+++ malonaldehyde.py
@@ -9,7 +9,7 @@ ndim = 3
 # Input file and list of atom numbers surrounding stereogenic center
 input_path = './testdata/malonaldehyde_IRC.xyz'
 newdata = './examples/malonaldehyde_irc2.xyz'
-newdata2 = './examples/malonaldehyde_irc2.xyz'
+newdata2 = './examples/malonaldehyde_manipulated.xyz'
 stereo_atoms_malon = [4, 7, 1, 8]
 points_to_circle = [0, 12, 24]
 output_directory = './output'
@@ -21,7 +21,7 @@ system_name, output_directory, pca, pca_fit, pca_components, mean, values, lengt
     dim_red.pathreducer(input_path, ndim, input_type="Cartesians")
 
 
-new_data_df = dim_red.transform_new_data(newdata, './newdata', 2, pca_fit,
+new_data_df = dim_red.transform_new_data(newdata2, './newdata', 2, pca_fit,
                                          pca_components, mean, aligned_coords, stereo_atoms=stereo_atoms_malon,  input_type='Cartesians')[1]
 
 
