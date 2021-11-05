15f692c3a6712f26d64865d566ce4df5574c4a09
Lisa Hetzel <lisa_hetzel@freenet.de>
2021-11-05 11:42:42 +0100

    test with structure from irc

--- malonaldehyde.py
+++ malonaldehyde.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import dimensionality_reduction_functions as dim_red
-from plotting_functions import colored_line_and_scatter_plot, colored_line_and_scatter_plot, colored_line_plot_projected_data
+from plotting_functions import colored_line_and_scatter_plot, colored_line_and_scatter_plot, colored_line_plot_projected_data2
 
 # Number of PCA components
 ndim = 3
@@ -8,8 +8,9 @@ ndim = 3
 ############################################### EXAMPLE 1: MALONALDEHYDE ###############################################
 # Input file and list of atom numbers surrounding stereogenic center
 input_path = './testdata/malonaldehyde_IRC.xyz'
-newdata = './examples/malonaldehyde_irc2.xyz'
-newdata2 = './examples/malonaldehyde_manipulated.xyz'
+newdata = './testdata/malonaldehyde_irc2.xyz'
+newdata2 = './testdata/malonaldehyde_manipulated.xyz'
+newdata3 = './testdata/malonaldehyde_from_irc.xyz'
 stereo_atoms_malon = [4, 7, 1, 8]
 points_to_circle = [0, 12, 24]
 output_directory = './output'
@@ -21,20 +22,20 @@ system_name, output_directory, pca, pca_fit, pca_components, mean, values, lengt
     dim_red.pathreducer(input_path, ndim, input_type="Cartesians")
 
 
-new_data_df = dim_red.transform_new_data(newdata2, './newdata', 2, pca_fit,
+new_data_df = dim_red.transform_new_data(newdata3, './newdata', 2, pca_fit,
                                          pca_components, mean, aligned_coords, stereo_atoms=stereo_atoms_malon,  input_type='Cartesians')[1]
 
 
 pca_df = pd.DataFrame(pca)
 
-
+colored_line_and_scatter_plot(pca_df[0], y=pca_df[1], y1=pca_df[2], )
 # Original Data
 # colored_line_and_scatter_plot(pca_df[0], y=pca_df[1], output_directory=output_directory,
 # imgname=(system_name + "_Distances_noMW_scatterline"), points_to_circle=points_to_circle)
 
 # New Data
-colored_line_plot_projected_data(x=pca_df[0], y=pca_df[1], z=pca_df[2], new_data_x=new_data_df[0], new_data_y=new_data_df[1], new_data_z=new_data_df[2],
-                                 points_to_circle=points_to_circle)
+colored_line_plot_projected_data2(x=pca_df[0], y=pca_df[1], z=pca_df[2], new_data_x=new_data_df[0], new_data_y=new_data_df[1], new_data_z=new_data_df[2],
+                                  points_to_circle=points_to_circle)
 
 
 '''
